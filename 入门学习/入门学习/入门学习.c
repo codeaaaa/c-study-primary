6cfc9a02c6543ab2c9ce75a2c62f195fe2f6a927
codeaaaa <1664710074@qq.com>
2020-11-05 22:52:17 +0800

    入门学习

---
+++ 入门学习/入门学习/入门学习.c
@@ -0,0 +1,21 @@
+//包含一个叫stdio.h的文件
+//std - 标准 standard input output
+
+#include <stdio.h>
+int main()//主函数 - 程序的入口 - mian函数有且仅有一个
+{
+	//这里完成任务
+	//printf - print funtion - 打印函数
+	//库函数 - C语言本身提供给我们使用的函数
+	//使用别人的东西，打招呼
+	//#include
+	printf("hellow world\n");
+	return 0;
+}
+
+
+//这种写法是过时写法
+//void main()
+//{
+//
+//}
