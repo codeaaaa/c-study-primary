2f78623386c91cd74f0e8907a0680bf6208e3ff6
codeaaaa <1664710074@qq.com>
2020-11-07 09:35:22 +0800

    入门学习6

---
+++ 入门学习6/入门学习6/入门学习6.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	//if—°‘Ò”Ôæ‰
+	int input = 0;
+	int line = 0;
+	scanf("%d", &input);
+	if (input == 1)
+		printf("11111\n");
+	else
+		printf("0000\n");
+	//while—≠ª∑
+	while(line < 1000)
+	{
+		printf("666666  %d\n", line);
+		line++;
+	}
+
+
+
+	return 0;
+}
