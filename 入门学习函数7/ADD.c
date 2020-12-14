9167e37899db3d5a4deb93527b4a9d44dae5dd3b
codeaaaa <1664710074@qq.com>
2020-12-14 12:46:42 +0800

    入门学习函数7

---
+++ 入门学习函数7/ADD.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+//函数的定义
+int ADD(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
---
+++ 入门学习函数7/text.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include"ADD.h"
+//ÉùÃ÷º¯Êý
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = 0;
+	sum = ADD(a, b);
+	printf("%d\n", sum);
+	return 0;
+}
